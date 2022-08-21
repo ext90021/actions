6cc4939ae470fc74bb394f2a31e42ee6ed657960
Rafael Palomino <rafael.palomino-martinez@allianz.de>
2022-08-21 11:10:24 +0200

    Remove unused options

--- .github/actions/ansible-playbook/main.js
+++ .github/actions/ansible-playbook/main.js
@@ -7,7 +7,6 @@ const os = require('os')
 async function main() {
     try {
         const playbook = core.getInput("playbook", { required: true })
-        const requirements = core.getInput("requirements")
         const directory = core.getInput("directory")
         const key = core.getInput("key")
         const inventory = core.getInput("inventory")
@@ -15,8 +14,6 @@ async function main() {
         const vaultPassword = core.getInput("vault_password")
         const knownHosts = core.getInput("known_hosts")
         const options = core.getInput("options")
-        const sudo    = core.getInput("sudo")
-        const noColor = core.getInput("no_color")
 
         let cmd = ["ansible-playbook", playbook]
 
@@ -27,20 +24,6 @@ async function main() {
         if (directory) {
             process.chdir(directory)
             core.saveState("directory", directory)
-        }
-
-        if (requirements) {
-            const requirementsContent = fs.readFileSync(requirements, 'utf8')
-            const requirementsObject = yaml.parse(requirementsContent)
-
-            if (Array.isArray(requirementsObject)) {
-                await exec.exec("ansible-galaxy", ["install", "-r", requirements])
-            } else {
-                if (requirementsObject.roles)
-                    await exec.exec("ansible-galaxy", ["role", "install", "-r", requirements])
-                if (requirementsObject.collections)
-                    await exec.exec("ansible-galaxy", ["collection", "install", "-r", requirements])
-            }
         }
 
         if (key) {
@@ -79,16 +62,6 @@ async function main() {
             process.env.ANSIBLE_HOST_KEY_CHECKING = "True"
         } else {
             process.env.ANSIBLE_HOST_KEY_CHECKING = "False"
-        }
-
-        if (sudo) {
-            cmd.unshift("sudo", "-E", "env", `PATH=${process.env.PATH}`)
-        }
-
-        if (noColor) {
-            process.env.ANSIBLE_NOCOLOR = "True"
-        } else {
-            process.env.ANSIBLE_FORCE_COLOR = "True"
         }
 
         let output = ""
